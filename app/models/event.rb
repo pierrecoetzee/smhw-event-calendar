324e89b9a181b6d6f9cb2f57bb33acbef0d37bda
Pierre Coetzee <pierre.coetzee@gmail.com>
2016-06-05 22:26:01 +0100

    add the event specs and views

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,28 @@
 class Event < ActiveRecord::Base
+	
+	validates_presence_of :title, :subject
+	validate :check_dates
+	
+	private
+	def check_dates
+		unless self.start_date.present? 
+			self.errors.add(:start_date,'must be provided')
+			return
+		end
+
+		unless self.end_date.present? 
+			self.errors.add(:end_date,'must be provided')
+			return
+		end
+
+		if self.start_date > self.end_date
+			self.errors.add(:start_date, 'needs to be before end date')
+		end
+
+		if self.end_date < self.start_date
+			self.errors.add(:end_date, 'needs to be after start date')
+		end
+		
+	end
+	
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,84 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do 
+
+  	let(:event){ build(:event) }
+
+    describe 'dates' do 
+      context 'when no start date is set' do 
+
+        before do
+          event.update_attributes(start_date: nil)
+          event.update_attributes(end_date: Date.today)
+        end
+
+        it 'has an error message' do 
+          event.save
+          expected = ['Start date must be provided']
+          expect(event.errors.full_messages_for(:start_date)).to eq(expected)
+        end
+      end
+      
+      context 'when no end date is set' do 
+
+        before do
+          event.update_attributes(start_date: Date.today)
+          event.update_attributes(end_date: nil)
+        end
+
+        it 'has an error message' do 
+          event.save
+          expected = ['End date must be provided']
+          expect(event.errors.full_messages_for(:end_date)).to eq(expected)
+        end
+      end      
+    end
+
+
+  	describe '#start_date and #end_at' do
+  		context 'when the start date is after the end date' do 
+
+  			before do
+  				event.update_attributes(start_date: Date.today + 7.days)
+  				event.update_attributes(end_date: Date.today)
+  			end
+
+  			it 'should not be valid' do 
+  				event.save
+  				expect(event.valid?).to be(false)
+  			end
+
+  			it 'should contain the error messages for both dates' do 
+  				event.save
+  				expected = {:start_date =>["needs to be before end date"], 
+                      :end_date=>["needs to be after start date"]}.freeze
+     
+					expect(event.errors.messages).to eq(expected)
+  			end
+  		end
+
+      context 'when the end date is before the start date' do 
+
+        before do
+          event.update_attributes(start_date: Date.today)
+          event.update_attributes(end_date: Date.today - 7.days)
+        end
+
+        it 'should not be valid' do 
+          event.save
+          expect(event.valid?).to be(false)
+        end
+
+        it 'should contain the error messages for both dates' do 
+          event.save
+          expected = {:start_date=>["needs to be before end date"], 
+                      :end_date=>["needs to be after start date"]}.freeze
+     
+          expect(event.errors.messages).to eq(expected)
+        end
+      end      
+  	end
+  end
 end
+
